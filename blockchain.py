4b28d84df5de48e184e0f8f9b3cf08af4407935e
Timothy Dao <timster78811@gmail.com>
2018-10-22 15:03:55 -0400

    added a for loop

--- blockchain.py
+++ blockchain.py
@@ -24,4 +24,8 @@ add_value(last_transaction=get_last_blockchain_value(),
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print(blockchain)
+for block in blockchain:
+    print('Outputting Block')
+    print(block)
+
+print('Done!')
