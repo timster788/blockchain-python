6f53a88a1ced71fa03dc70361ac1089797ef460d
Timothy Dao <timster78811@gmail.com>
2018-10-24 16:43:04 -0400

    added a list of participants

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,12 @@ genesis_block = {
 }
 blockchain = [genesis_block]
 open_transactions = []
-owner = 'Timothy'
+owner = 'Max'
+participants = {'Max'}
+
+
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
 
 
 def get_last_blockchain_value():
@@ -16,7 +21,7 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 # This function accepts two arguments.
-# One required one (transaction_amount) and one o ptional one (last_transaction)
+# One required one (transaction_amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
 
 
@@ -24,9 +29,9 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
-        :sender: sender of the coins.
-        :recipient: person who gets the coins.
-        :amount: The amount of coins sent with the transactions (default = 1.0 coins).
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction (default = 1.0)
     """
     transaction = {
         'sender': sender,
@@ -34,13 +39,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
-# adds a new block to block chain
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
-    print(hashed_block)
+    hashed_block = hash_block(last_block)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
@@ -52,17 +57,19 @@ def mine_block():
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it in user_input
-    tx_recipient = input('Enter the recipient of the transaction:')
+    tx_recipient = input('Enter the recipient of the transaction: ')
     tx_amount = float(input('Your transaction amount please: '))
-    return (tx_recipient, tx_amount)
+    return tx_recipient, tx_amount
 
 
 def get_user_choice():
+    """Prompts the user for its choice and return it."""
     user_input = input('Your choice: ')
     return user_input
 
 
 def print_blockchain_elements():
+    """ Output all blocks of the blockchain. """
     # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting Block')
@@ -72,62 +79,60 @@ def print_blockchain_elements():
 
 
 def verify_chain():
-    # block_index = 0
-    is_valid = True
-    for block_index in range(len(blockchain)):
-        if block_index == 0:
-            # If we're checking the first block, we should skip the iteration (since there's no previous block)
+    """ Verify the current blockchain and return True if it's valid, False otherwise."""
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-        # Check the previous block (the entire one) vs the first element of the current block
-        elif blockchain[block_index][0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-        # for block in blockchain:
-        #     if block_index == 0:
-        #         block_index += 1
-        #         continue
-        #     elif block[0] == blockchain[block_index - 1]:
-        #         is_valid = True
-        #     else:
-        #         is_valid = False
-        #         break
-        #     block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 
 waiting_for_input = True
 
+# A while loop for the user input interface
+# It's a loop that exits once waiting_for_input becomes False or when break is called
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_data = get_transaction_value()
         recipient, amount = tx_data
-        # add transaction amount to the blockchain
+        # Add the transaction amount to the blockchain
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
-
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
+        # Make sure that you don't try to "hack" the blockchain if it's empty
         if len(blockchain) >= 1:
-            blockchain[0] = [2]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [{'sender': 'Chris', 'recipient': 'Max', 'amount': 100.0}]
+            }
     elif user_choice == 'q':
+        # This will lead to the loop to exist because it's running condition becomes False
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
-    print('Choice registered!')
-    # if not verify_chain():
-    #     print('Invalid blockchain')
-    #     break
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        # Break out of the loop
+        break
 else:
-    print('User left')
+    print('User left!')
+
+
 print('Done!')
